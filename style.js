092d5308eb850fada12c93d46238b443996fdcc8
Trung Luu Hoang <trung.luuhoang@pycogroup.com>
2020-08-16 16:46:00 +0700

    Update menu

---
+++ style.js
@@ -0,0 +1,12 @@
+function activeMenu() {
+  let element = document.querySelector(".menu-mobile .menu");
+  element.classList.toggle("active");
+}
+
+function scrollToTop() {
+  window.scrollTo({ scrollTop: 0 }, "slow");
+}
+
+// function setActive() {
+
+// }
